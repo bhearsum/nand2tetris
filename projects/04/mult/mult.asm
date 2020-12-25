7487e11ca72d858cf5facff75236c4b29e9b3bb4
Ben Hearsum <ben@hearsum.ca>
2020-12-24 21:03:39 -0500

    Implement multiplication

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,27 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+    @R2
+    M=0
+(LOOP)
+    // end if R1 is zero
+    @R1
+    D=M
+    @DONE
+    D;JEQ
+
+    // increase R2 by R0 value
+    @R0
+    D=M
+    @R2
+    M=M+D
+
+    // decrease R1 by 1
+    @R1
+    M=M-1
+
+    // loop
+    @LOOP
+    D;JMP
+    
+(DONE)
