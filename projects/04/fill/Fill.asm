d953d7b7332c48e3ae9e4c8d7b954530d5cf4537
Ben Hearsum <ben@hearsum.ca>
2020-12-24 22:48:10 -0500

    Implement Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,63 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(POLL)
+    @KBD
+    D=M
+    @DRAWBLACK
+    D;JNE       // Draw black if there's a value in the keyboard register
+    @DRAWWHITE
+    0;JMP       // Otherwise, draw white
+
+(DRAWBLACK)
+    @R0
+    M=0      // R0 tracks the current screen pixel we're at
+
+(BLACKLOOP)
+    @R0         // Go back to polling if we've reached the last pixel
+    D=M
+    @8192
+    D=A-D
+    @POLL
+    D;JEQ
+
+    @R0
+    D=M
+    @SCREEN
+    A=A+D
+    M=-1         // Otherwise draw black to the current pixel
+                 // This is negative one because "1" is the value
+                 // for black, and the screen's memory maps are
+                 // 16-bit values, and -1's binary representation is all 1s.
+
+    @R0
+    M=M+1
+
+    @BLACKLOOP
+    0;JMP
+
+
+(DRAWWHITE)
+    @R0
+    M=0      // R0 tracks the current screen pixel we're at
+
+(WHITELOOP)
+    @R0         // Go back to polling if we've reached the last pixel
+    D=M
+    @8192
+    D=A-D
+    @POLL
+    D;JEQ
+
+    @R0
+    D=M
+    @SCREEN
+    A=A+D
+    M=0         // Otherwise draw white to the current pixel
+
+    @R0
+    M=M+1
+
+    @WHITELOOP
+    0;JMP
